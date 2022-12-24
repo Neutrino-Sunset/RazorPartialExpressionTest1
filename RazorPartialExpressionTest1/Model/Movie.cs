85a6a1f1de3cadee440c054a98c4b3dd61749eee
NeutrinoSunset <18445931+Neutrino-Sunset@users.noreply.github.com>
2022-12-24 13:21:31 +0000

    Add project files.

---
+++ RazorPartialExpressionTest1/Model/Movie.cs
@@ -0,0 +1,11 @@
+﻿namespace RazorPartialExpressionTest1.Model
+{
+   public class Movie
+   {
+      public string Title { get; set; }
+
+      public DateTime ReleaseDate { get; set; }
+
+      public string Genre { get; set; }
+   }
+}
---
+++ RazorPartialExpressionTest1/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using RazorPartialExpressionTest1.Model;
+
+namespace RazorPartialExpressionTest1.Pages
+{
+   public class IndexModel : PageModel
+   {
+      [BindProperty]
+      public Movie Movie { get; set; }
+
+      private readonly ILogger<IndexModel> _logger;
+
+      public IndexModel(ILogger<IndexModel> logger)
+      {
+         _logger = logger;
+      }
+
+      public void OnGet()
+      {
+
+      }
+
+      public void OnPost()
+      {
+         _logger.LogInformation(Movie.ToString());
+      }
+   }
+}
